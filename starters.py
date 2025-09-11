1a91386ffdc3f78bea1bf870a5696bd38a1c2cf2
ajay-nagyal <anagyal@insilica.co>
2025-09-11 15:21:12 -0400

    feat(starts): starter prompts

---
+++ starters.py
@@ -0,0 +1,23 @@
+import chainlit as cl
+
+
+@cl.set_starters
+async def set_starters():
+    return [
+        cl.Starter(
+            label="List bricks",
+            message="Get me a list of bricks with their descriptions",
+        ),
+        cl.Starter(
+            label="Find human genome bricks",
+            message="Get me a list of bricks discussing the human genome",
+        ),
+        cl.Starter(
+            label="Clinical variations information",
+            message="I want some information on clinical variations, so find me a brick that will handle that",
+        ),
+        cl.Starter(
+            label="Side effects of tylenol",
+            message="What are some side effects of tylenol, according to the bricks?",
+        ),
+    ]
